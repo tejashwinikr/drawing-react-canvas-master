eff0692c1b542d29368de92018587191be122561
tejashwinikr <ahwiniteju@gmail.com>
2023-03-26 20:16:09 +0530

    pic drawing

--- src/App.js
+++ src/App.js
@@ -1,40 +1,33 @@
-import React from 'react'
-import { useState } from "react";
+import React, { useState } from 'react'
 import { Canvas } from './Canvas'
 import { ClearCanvasButton } from './ClearCanvasButton';
 
 function App() {
-  const [imgs,setImgs] =useState()
-  const [imageuploaded,setImageUploaded] = useState();
+  const [imgs, setImgs] = useState()
 
+  const handleFile = (e) => {
 
-  const handleFile =(e) =>{
-    setImageUploaded(e.target.files)
-    console.log(e.target.files);
-    console.log(imageuploaded)
-   
-    console.log(e.target.files)
-        const data = new FileReader()
-        data.addEventListener('load',()=>{
-            setImgs(data.result)
-        })
-        data.readAsDataURL(e.target.files[0])
-    }
+    const data = new FileReader()
+    data.addEventListener('load', () => {
+      setImgs(data.result)
+    })
+    data.readAsDataURL(e.target.files[0])
+  }
 
-    console.log(imgs)
+  //console.log(imgs)
   return (
     <>
-      <form>
-      <input type="file" name="picture" onChange={e => handleFile(e)}>
-      
-      </input>
-    </form>
- 
+      <form className='file-form'>
+        <input type="file" name="picture" onChange={e => handleFile(e)}>
+        </input>
+      </form>
+      <img src={imgs} alt="imag" height="200px" width="200px">
+      </img>
+      <div className='canvas-container'>
+        <Canvas image={imgs} />
+        <ClearCanvasButton />
+      </div>
 
-    <img src={imgs} alt="imag" height="200px" width="200px">
-    </img>
-      <Canvas image={imgs}/>
-      <ClearCanvasButton/>
     </>
   );
 }
--- src/Canvas.js
+++ src/Canvas.js
@@ -3,7 +3,6 @@ import { useCanvas } from "./CanvasContext";
 
 export function Canvas(props) {
   const image = props;
-  console.log(image)
   const {
     canvasRef,
     prepareCanvas,
@@ -16,13 +15,38 @@ export function Canvas(props) {
     prepareCanvas();
   }, []);
 
+  useEffect(() => {
+    copyImageToCanvas()
+  }, [image]);
+
+  function copyImageToCanvas() {
+    let image = document.querySelector("img");
+
+    let canvas = document.querySelector("canvas");
+
+    let ctx = canvas.getContext("2d");
+
+    ctx.drawImage(image, 0, 0,);
+
+  }
+
+  const saveImageToLocal = (event) => {
+    let link = event.currentTarget;
+    link.setAttribute('download', 'canvas.png');
+    let image = canvasRef.current.toDataURL('image/png');
+    link.setAttribute('href', image);
+  };
+
   return (
-    <canvas
-      onMouseDown={startDrawing}
-      onMouseUp={finishDrawing}
-      onMouseMove={draw}
-      ref={canvasRef}
-      image={image}
-    />
+    <div>
+      <canvas
+        onMouseDown={startDrawing}
+        onMouseUp={finishDrawing}
+        onMouseMove={draw}
+        ref={canvasRef}
+      >
+      </canvas>
+      <a id="download_image_link" href="download_link" onClick={saveImageToLocal}>Download Image</a>
+    </div>
   );
 }
